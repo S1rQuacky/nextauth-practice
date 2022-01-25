429fdd536597accc22b4d3356cb06e9641b3efa5
Neide Urbina <neide.n.urbina@gmail.com>
2022-01-25 13:38:31 -0600

    working next auth practice

---
+++ pages/login.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import Link from "next/link";
+// import {signIn, signOut, useSession} from "next-auth/client";
+
+// export default function Login() {
+//     const [session, loading] = useSession();
+
+//     <main className={StyleSheet.main}
+// }
